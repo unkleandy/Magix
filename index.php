ce455370d9e23dea52463f6ef604a56f1a10900b
Unkle Andy <38595124+unkleandy@users.noreply.github.com>
2020-05-04 11:26:24 -0400

    JS animations added

--- index.php
+++ index.php
@@ -12,24 +12,36 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Magix Connexion</title>
+	<script src="js/sprite/Intro.js"></script>
+	<script src="js/sprite/Sword.js"></script>
+	<script src="js/sprite/LoginBox.js"></script>
+	<script src="js/index.js"></script>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
-<body>
-	<div class="login">
+<body class="index">
+<div class=intro_title>
+	<div class="intro_magix_tm"></div>
+	<div class="intro_ie"></div>
+	<div class="intro_sword"></div>
+</div>
+
+	<div class= "login_box" ></div>
 		<form action="index.php" method="post">
-			<div>
-				<label>Nom d'usager</label>
-				<input type="text" name="username" value="<?= $data["username"] ?>" id="username" required="">
-			</div>
-			<div>
-				<label>Mot de passe : </label>
-				<input type="password" name="password" id="pwd" required="">
+			<div class="login">
+				<div>
+					<label>Nom d'usager</label>
+					<input type="text" name="username" value="<?= $data["username"] ?>" id="username" required="">
+				</div>
+				<div>
+					<label>Mot de passe : </label>
+					<input type="password" name="password" id="pwd" required="">
+				</div>
 			</div>
 			<div>
 				<button class="btn-magix" type="submit">Connexion</button>
 			</div>
 		</form>
-	</div>
-
-
-</body></html>
+		
+	</body>
+	</html>
